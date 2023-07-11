de1f1b784f798609951b5b1247afe726059ac8b8
Bayu K <50491719+bayukembara@users.noreply.github.com>
2023-07-12 00:56:02 +0700

    Storing and Fetching Data

--- models/product.js
+++ models/product.js
@@ -5,7 +5,8 @@
 // }
 
 // * this is model for ES6
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
     constructor(t) {
@@ -13,9 +14,33 @@ module.exports = class Product {
     }
     // * this is function save without write function in front of the function name
     save() {
-        products.push(this);
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            "data",
+            "products.json"
+        );
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            "data",
+            "products.json"
+        );
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
     }
 };
